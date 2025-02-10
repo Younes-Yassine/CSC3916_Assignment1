3644ead7d62e10997fb8919d98a2b54201dae398
Younes-Yassine <156845678+Younes-Yassine@users.noreply.github.com>
2025-02-09 22:07:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.post("/echo", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => { 
     console.log(`Echo server is running on port ${PORT}`);
 });
